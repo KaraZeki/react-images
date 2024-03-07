338849cb6238dff3b9da4afb015d44bfb5a459ac
Mehmet Zeki KARA <karamehmetzeki506@gmail.com>
2024-03-07 23:03:08 +0300

    css configuration

--- src/Components/ImageItem.js
+++ src/Components/ImageItem.js
@@ -1,8 +1,9 @@
+import "./ImageItem.css";
 function ImageItem(img) {
-  const imageUrl =img.image.urls.small;
+  const imageUrl = img.image.urls.small;
   return (
-    <div className='imageDiv'>
-      <img className='Img' src={imageUrl} alt=""></img>
+    <div className="imageDiv">
+      <img className="Img" src={imageUrl} alt=""></img>
     </div>
   );
 }
--- src/Components/ImagesList.js
+++ src/Components/ImagesList.js
@@ -1,8 +1,9 @@
 import ImageItem from "./ImageItem";
+import "./ImageLis.css";
 
 function ImagesList({ imagesPlaceHolder }) {
   return (
-    <div>
+    <div className="imageList">
       {imagesPlaceHolder.map((img, index) => {
         return <ImageItem key={index} image={img}></ImageItem>;
       })}
